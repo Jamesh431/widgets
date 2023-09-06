28aba75aed7dc61e14a6aa953d6ae9f9bb9834b6
james <jdhales2686@gmail.com>
2023-09-06 16:06:10 -0600

    added image slider

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import ImageSlider from "./widgets/imageSlider/ImageSlider";
+import "../styles/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Widgets by James</h1>
+      <div className="widgets">
+        <ImageSlider />
+      </div>
+    </div>
+  );
+}
+
+export default App;
